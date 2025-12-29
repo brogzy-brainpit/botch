3d2a5271870abcb8ad5fc1c5d186b74a37551dfd
unknown <dangabarin2020@gmail.com>
2025-12-29 14:32:58 +0100

    update commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import { AnimatePresence, useInView,motion } from "framer-motion";
 import { useRouter } from "next/router";
 import { useEffect, useRef } from "react";
 // import { Instrument_Serif,Instrument_Sans, Lora, DM_Sans,Manrope,Agdasima,Epilogue,Poppins } from "next/font/google";
-import {Manrope,Agdasima } from "next/font/google";
+import {Manrope,Agdasima, Epilogue } from "next/font/google";
 
 // import "../styles/fonts.css";
 import "../styles/mostHave.css";
@@ -20,14 +20,14 @@ const custom2 = localFont({
   variable: "--font-custom2",
   weight: "100 200 300 400 500 600 700 800 900",
 });
-const custom = localFont({
-  src:  "./fonts/AktivGrotesk-Bold.otf",
-  // src:  "./fonts/MonumentExtended-Ultrabold.otf",
+// const custom = localFont({
+//   src:  "./fonts/AktivGrotesk-Bold.otf",
+//   // src:  "./fonts/MonumentExtended-Ultrabold.otf",
 
-  // weight: "100 200 300 400 500 600 700 800 900",
-  variable: "--font-custom",
+//   // weight: "100 200 300 400 500 600 700 800 900",
+//   variable: "--font-custom",
  
-});
+// });
 const body= localFont({
   src:  "./fonts/NeueMontreal-Regular.otf",
 
@@ -35,13 +35,13 @@ const body= localFont({
   variable: "--font-body",
  
 });
-// const body = Manrope({
-//   subsets: ["latin"],
-//   weight: ["400", "500", "700"],
-//   style: ["normal", ], // 👈 include italics
-//   variable: "--font-body",
-//   display: "swap",
-// });
+const custom = Epilogue({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  style: ["normal", ], // 👈 include italics
+  variable: "--font-body",
+  display: "swap",
+});
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
